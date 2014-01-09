0365f719d7f958328ca8e0634fb34502518664f8
Sergey Polishchuk <mayorserega@gmail.com>
2014-01-09 15:34:44 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Video::Application.config.secret_key_base = 'fb76f820a857343fe3f7e29110bd8a48f4535f09cb8c76db1f77b6b1bdf5e5d5cafcc5a63420c582cfaf3002899a49a3b1cb7bd1ccdbb853ec5af389b1b85df4'
