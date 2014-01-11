1677e3d6f3f540ce4b5d80e41b99e4b140e1f720
Sergey Polishchuk <mayorserega@gmail.com>
2014-01-11 22:55:23 +0200

    added module omni_auth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,10 @@
+require 'omni_auth'
+
 class SessionsController < ApplicationController
   def create
     user = User.from_omniauth(env["omniauth.auth"])
     session[:user_id] = user.id
+    OmniAuth.get_vk_access_token(ENV['API_KEY'], ENV['API_SECRET'], params[:code])
     redirect_to root_url
   end
 
